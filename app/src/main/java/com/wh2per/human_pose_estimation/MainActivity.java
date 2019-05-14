cd407eaa44fa53fcfedc55cf735cb729eaf2f930
wh2per <93negaro@gmail.com>
2019-05-14 23:36:45 +0900

    Implement MainActivity and WebviewActivity

--- app/src/main/java/com/wh2per/human_pose_estimation/MainActivity.java
+++ app/src/main/java/com/wh2per/human_pose_estimation/MainActivity.java
@@ -1,13 +1,33 @@
 package com.wh2per.human_pose_estimation;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startbutton;
+    private Context cont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cont = getApplicationContext();
+
+        startbutton = findViewById(R.id.startbutton);
+        startbutton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(cont, WebViewActivity.class);
+                startActivity(intent);
+                Log.e("a","aaa");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/wh2per/human_pose_estimation/WebViewActivity.java
@@ -0,0 +1,45 @@
+package com.wh2per.human_pose_estimation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+    private WebView mWebView;
+    private String myUrl = "https://www.github.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        // 웹뷰 셋팅팅
+        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mWebView.loadUrl("http://www.github.com/");
+    }
+    // 뒤로가기 버튼
+    @Override
+    public boolean onKeyDown (int keyCode, KeyEvent event){
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mWebView.canGoBack()) {
+                mWebView.goBack();
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
