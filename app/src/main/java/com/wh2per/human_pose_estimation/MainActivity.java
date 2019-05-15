ff76e131a97ac5c1e604ad534b501ce15887166c
wh2per <93negaro@gmail.com>
2019-05-15 15:11:26 +0900

    Implement Video Record in MainActivity

--- app/src/main/java/com/wh2per/human_pose_estimation/MainActivity.java
+++ app/src/main/java/com/wh2per/human_pose_estimation/MainActivity.java
@@ -1,8 +1,13 @@
 package com.wh2per.human_pose_estimation;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,7 +16,8 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button startbutton;
+    private Button uploadButton;
+    private Button recordButton;
     private Context cont;
 
     @Override
@@ -21,15 +27,49 @@ public class MainActivity extends AppCompatActivity {
 
         cont = getApplicationContext();
 
-        startbutton = findViewById(R.id.startbutton);
-        startbutton.setOnClickListener(new Button.OnClickListener() {
+        // 6.0 마쉬멜로우 이상일 경우에는 권한 체크 후 권한 요청
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED ) {
+                Log.d("설정 : ", "권한 설정 완료");
+            } else {
+                Log.d("설정 : ", "권한 설정 요청");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+
+
+
+        uploadButton = findViewById(R.id.uploadButton);
+        uploadButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(cont, WebViewActivity.class);
                 //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.88/upload/application/sk/upload.php"));
                 startActivity(intent);
-                Log.e("a","aaa");
+                Log.e("upload"," : start");
+            }
+        });
+
+        recordButton = findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(cameraIntent, 1);
+                Log.e("record"," : start");
             }
         });
     }
+
+    // 권한 요청
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d("권한 요청 : ", "onRequestPermissionsResult");
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED ) {
+            Log.d("권한 요청 : ", "Permission: " + permissions[0] + "was " + grantResults[0]);
+        }
+    }
+
 }
