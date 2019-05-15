4ca84df01afe7b295cd58072a3116781f81f0549
wh2per <93negaro@gmail.com>
2019-05-15 13:32:41 +0900

    Implement FIlechooser in WebViewActivity

--- app/src/main/java/com/wh2per/human_pose_estimation/WebViewActivity.java
+++ app/src/main/java/com/wh2per/human_pose_estimation/WebViewActivity.java
@@ -1,16 +1,22 @@
 package com.wh2per.human_pose_estimation;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
     private WebView mWebView;
-    private String myUrl = "https://www.github.com";
+    private String myUrl = "http://192.168.0.88/upload/application/up/upload.php/";
+    private ValueCallback<Uri> filePathCallbackNormal;
+    private ValueCallback<Uri[]> filePathCallbackLollipop;
+    private final static int FILECHOOSER_NORMAL_REQ_CODE = 1;
+    private final static int FILECHOOSER_LOLLIPOP_REQ_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +25,52 @@ public class WebViewActivity extends AppCompatActivity {
 
         // 웹뷰 셋팅팅
         mWebView = (WebView) findViewById(R.id.webView);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setLoadWithOverviewMode(true);
 
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            // For Android < 3.0
+            public void openFileChooser( ValueCallback<Uri> uploadMsg) {
+                Log.d("MainActivity", "3.0 <");
+                openFileChooser(uploadMsg, "");
+            }
+            // For Android 3.0+
+            public void openFileChooser( ValueCallback<Uri> uploadMsg, String acceptType) {
+                Log.d("MainActivity", "3.0+");
+                filePathCallbackNormal = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                // i.setType("video/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_NORMAL_REQ_CODE);
+            }
+            // For Android 4.1+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                Log.d("MainActivity", "4.1+");
+                openFileChooser(uploadMsg, acceptType);
+            }
+
+            // For Android 5.0+
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    WebChromeClient.FileChooserParams fileChooserParams) {
+                Log.d("MainActivity", "5.0+");
+                if (filePathCallbackLollipop != null) {
+                    filePathCallbackLollipop.onReceiveValue(null);
+                    filePathCallbackLollipop = null;
+                }
+                filePathCallbackLollipop = filePathCallback;
+
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+               // i.setType("video/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_LOLLIPOP_REQ_CODE);
+
                 return true;
             }
         });
-        mWebView.loadUrl("http://www.github.com/");
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.loadUrl(myUrl);
     }
     // 뒤로가기 버튼
     @Override
@@ -42,4 +83,30 @@ public class WebViewActivity extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == FILECHOOSER_NORMAL_REQ_CODE) {
+                if (filePathCallbackNormal == null) return ;
+                Uri result = (data == null || resultCode != RESULT_OK) ? null : data.getData();
+                filePathCallbackNormal.onReceiveValue(result);
+                Log.e("Normal : ",""+ filePathCallbackNormal+"");
+                filePathCallbackNormal = null;
+            } else if (requestCode == FILECHOOSER_LOLLIPOP_REQ_CODE) {
+                if (filePathCallbackLollipop == null) return ;
+                filePathCallbackLollipop.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+                Log.e("Normal_LOl1 : ",""+ filePathCallbackLollipop+"");
+                filePathCallbackLollipop = null;
+            }
+        } else {
+            if (filePathCallbackLollipop != null) {
+                filePathCallbackLollipop.onReceiveValue(null);
+                Log.e("Normal_LOl2 : ",""+ filePathCallbackLollipop+"");
+                filePathCallbackLollipop = null;
+            }
+        }
+
+    }
+
 }
